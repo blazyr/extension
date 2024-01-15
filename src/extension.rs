b260618cdb96dabeee3fc1d777d16e2ac2af738a
Luca <coduri.vitor@gmail.com>
2024-01-15 16:06:50 +0100

    init

---
+++ src/extension.rs
@@ -0,0 +1,14 @@
+#[derive(Clone)]
+pub enum EntityType {
+    App,
+    Command,
+}
+
+#[derive(Clone)]
+pub struct Entity {
+    pub name: String,
+    pub description: Option<String>,
+    pub alias: Option<String>,
+    pub command: Rc<Box<dyn Fn() -> Result<()>>>,
+    pub r#type: EntityType,
+}
