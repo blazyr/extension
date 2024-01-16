e57eb416884b9660fd8e2f2612de6d79460d5241
Luca <coduri.vitor@gmail.com>
2024-01-16 19:04:58 +0100

    using abi_stable crate
    The rust abi isn't stable so we need this crate to make it easier to user the c-abi

--- src/extension.rs
+++ src/extension.rs
@@ -1,17 +1,26 @@
-use anyhow::Result;
-use std::rc::Rc;
+use crate::REntity;
 
-#[derive(Clone)]
-pub enum EntityType {
-    App,
-    Command,
-}
-
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Entity {
-    pub name: String,
+    id: u64,
+    pub name: Option<String>,
     pub description: Option<String>,
     pub alias: Option<String>,
-    pub command: Rc<Box<dyn Fn() -> Result<String>>>,
-    pub r#type: EntityType,
+}
+
+impl Entity {
+    pub fn get_id(&self) -> u64 {
+        self.id
+    }
+}
+
+impl From<REntity> for Entity {
+    fn from(value: REntity) -> Self {
+        Self {
+            id: value.id,
+            name: value.name.map(|v| v.to_string()).into(),
+            description: value.description.map(|v| v.to_string()).into(),
+            alias: value.alias.map(|v| v.to_string()).into(),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
 mod extension;
-pub use spotlight_macros::dispose;
-pub use spotlight_macros::entities;
-pub use spotlight_macros::entry_point;
-
 pub use extension::Entity;
-pub use extension::EntityType;
+
+use abi_stable::{
+    declare_root_module_statics,
+    library::RootModule,
+    package_version_strings,
+    sabi_types::VersionStrings,
+    std_types::{ROption, RString, RVec},
+    StableAbi,
+};
+
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(kind(Prefix))]
+pub struct Plugin {
+    #[sabi(last_prefix_field)]
+    pub entities: extern "C" fn() -> RVec<REntity>,
+    pub on_entity_action: extern "C" fn(u64) -> (),
+}
+
+impl RootModule for Plugin_Ref {
+    // The name of the dynamic library
+    const BASE_NAME: &'static str = "plugin";
+    // The name of the library for logging and similars
+    const NAME: &'static str = "plugin";
+    // The version of this plugin's crate
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
+
+    declare_root_module_statics! {Plugin_Ref}
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, PartialEq, Eq, StableAbi)]
+pub struct REntity {
+    pub id: u64,
+    pub name: ROption<RString>,
+    pub description: ROption<RString>,
+    pub alias: ROption<RString>,
+}
